b54199c4bb8150756a159baf1472b479c0c95782
Carlos Martinez <itscarlosisaac@gmail.com>
2019-04-15 10:07:55 -0400

    Added tabs icons.

---
+++ src/components/Toolbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Toolbar extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <aside className="app__toolbar">
+        Toolbar
+      </aside>
+    )
+  }
+}
