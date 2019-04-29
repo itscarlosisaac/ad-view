800284ce93e4bf75f1ea9d7ee7ba1a99bdd2edef
Carlos Martinez <itscarlosisaac@gmail.com>
2019-04-29 16:58:54 -0400

    Created options model.

---
+++ src/models/OptionsModel.js
@@ -0,0 +1,8 @@
+export default class OptionsModel {
+  constructor() {
+    this.is_size_editable = false;
+    this.is_param_editable = false;
+    this.use_size_as_param = true;
+    this.use_provider_as_param = true;
+  }
+}
